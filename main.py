1464146c0f958edc4afd0b7129d3959619d2876c
TAMAGO55 <tamago.55123@gmail.com>
2026-02-08 16:30:51 +0900

    send command

--- main.py
+++ main.py
@@ -31,6 +31,59 @@ async def main(bot:commands.Bot):
                 log.info(f"{len(synced)}個のコマンドを同期しました。")
             except Exception as e:
                 log.error(f"コマンドの同期中にエラーが発生しました。")
+                
+        class SendEmbedModal(discord.ui.Modal):
+            def __init__(self, channel:discord.TextChannel, message:str):
+                super().__init__(
+                    title="フォーム",
+                    timeout=None,
+                )
+    
+                self.messages = discord.ui.TextInput(
+                    label="Color Code",
+                    style=discord.TextStyle.short,
+                    max_length=6,
+                    required=False,
+                )
+                self.add_item(self.messages)
+    
+                self.channel = channel
+                self.message = message
+    
+            async def on_submit(self, interaction:discord.Interaction):
+                if self.messages.value:
+                    a = int(f"0x{self.messages.value}", 16)
+                else:
+                    a = None
+                await self.channel.send(embed=discord.Embed(description=self.message, color=a))
+                await interaction.response.send_message("sended.",ephemeral=True)
+    
+        @bot.tree.context_menu(name="メッセージを再送信")
+        async def message_re_send(interaction:discord.Interaction, message:discord.Message):
+            if interaction.user.guild_permissions.administrator:
+                await message.channel.send(content=message.content, embeds=message.embeds)
+                await interaction.response.send_message(content="sended.",ephemeral=True)
+            else:
+                await interaction.response.send_message(content="このアプリは、管理者のみ実行可能です。", ephemeral=True)
+    
+        @bot.tree.context_menu(name="メッセージを埋め込みに変換")
+        async def message_send_embed(interaction:discord.Interaction, message:discord.Message):
+            if interaction.user.guild_permissions.administrator:
+                modal = SendEmbedModal(channel=message.channel, message=message.content)
+                await interaction.response.send_modal(modal)
+            else:
+                await interaction.response.send_message(content="このアプリは、管理者のみ実行可能です。", ephemeral=True)
+    
+    @bot.tree.context_menu(name="埋め込みをメッセージに変換")
+    async def embed_send_message(interaction:discord.Interaction, message:discord.Message):
+        if interaction.user.guild_permissions.administrator:
+            a = ""
+            for i in message.embeds:
+                a = a + i.description
+            await message.channel.send(content=a)
+            await interaction.response.send_message("sended.", ephemeral=True)
+        else:
+            await interaction.response.send_message(content="このアプリは、管理者のみ実行可能です。", ephemeral=True)
 
         await bot.start(TOKEN)
     except Exception as e:
